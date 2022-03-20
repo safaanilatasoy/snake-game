5ef86100a28edc088cf9f0d30846715a6831b2f8
safaanilatasoy <safaanilatasoy@gmail.com>
2022-03-20 16:05:08 +0300

    main

---
+++ snake.js
@@ -0,0 +1,51 @@
+
+class Snake{
+    constructor(x, y, size){
+        this.x = x
+        this.y = y
+        this.size = size
+        this.tail = [{x:this.x, y:this.y}]
+        this.rotateX = 0
+        this.rotateY = 1
+
+    }
+    move(){
+        var newRect;
+        if(this.rotateX == 1){
+            newRect = {
+                x: this.tail[this.tail.length-1].x + this.size,
+                y: this.tail[this.tail.length-1].y
+            } 
+            }else if(this.rotateX == -1){
+                newRect = {
+                    x: this.tail[this.tail.length-1].x - this.size,
+                    y: this.tail[this.tail.length-1].y
+                }
+        }else if(this.rotateY == 1){
+            newRect = {
+                x: this.tail[this.tail.length-1].x,
+                y: this.tail[this.tail.length-1].y + this.size
+            }
+    } else if(this.rotateY == -1){
+        newRect = {
+            x: this.tail[this.tail.length-1].x,
+            y: this.tail[this.tail.length-1].y -this.size
+        }
+} 
+
+var canvas = document.getElementById('canvas');
+var snake = new Snake();
+var apple = new Apple();
+var canvasContext = canvas.getContext('2d');
+window.onload = () =>{
+    gameLoop();
+}
+
+function gameLoop(){
+    // fps value
+    setInterval(show, 1000/15) 
+}
+function show(){
+    update();
+    draw();
+}
